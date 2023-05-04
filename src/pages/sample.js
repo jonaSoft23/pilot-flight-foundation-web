5f996f3e28b293f61cabb86bad3d53c27a40bc88
jonaSoft23 <jonathanowot@gmail.com>
2023-05-04 18:50:01 +0300

    Devias template loaded

---
+++ src/pages/sample.js
@@ -0,0 +1,12 @@
+import Button from '@material-ui/core/Button';
+
+export default function sample() {
+    return(
+        <>
+            <h1>This is a sample page.</h1>
+            <Button variant="contained" color="primary"> 
+                hello
+            </Button>
+        </>
+    )
+}
